c5f4af4afc30fdb32fcb30da41c354e93c5ee699
Raj Patel <rajpatel.260303@gmail.com>
2026-06-23 13:49:04 +0530

    Middleware Bug Resolved

--- Backend/middleware/auth.middleware.js
+++ Backend/middleware/auth.middleware.js
@@ -5,25 +5,24 @@ export const authMiddleware = async(req,res,next)=>{
     try{
         const accessToken = req.cookies?.accessToken;
         if(!accessToken){
-            throw new NotFoundError("Access Token Missing");
+            return next(new NotFoundError("Access Token Missing"));
         }
         const decoded = jwt.verify(accessToken,process.env.ACCESS_SECRET);
         req.user = decoded;
         next();
     } catch(err){
-        throw new UnauthorizedError("Invalid or Expired Token");
+        return next(new UnauthorizedError("Invalid or Expired Token"));
     }
 };
 
 export const authoriseRoles = (...allowedRoles)=>{
     return (req,res,next)=>{
         if(!req.user){
-            throw new UnauthorizedError("User not Authenticated");
+            return next(new UnauthorizedError("User not Authenticated"));
         }
         if(!allowedRoles.includes(req.user.role)){
-            throw new ForbiddenError("You are not allowed to access this resource");
+            return next(new ForbiddenError("You are not allowed to access this resource"));
         }
         next();
     }
-
 };
